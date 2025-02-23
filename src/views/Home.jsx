b35eaa24db763984189c2a9f3f6173813208b923
Bobb58770 <dhruv.puri.202@k12.friscoisd.org>
2025-02-23 11:09:44 -0600

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center p-5 overflow-hidden">
       <Logo width="200px" height="200px" />
-      <h1 className="text-4xl font-bold my-5">Welcome to Starlight!</h1>
+      <h1 className="text-4xl font-bold my-5">Welcome to Probe Games!</h1>
       <p className="text-lg mb-5">
         Your favorite place for unblocked games! Enjoy a wide selection of fun
         and exciting games without any restrictions.
@@ -24,7 +24,7 @@ function Home() {
       <BannerAd />
       <footer className="mt-10 fixed bottom-0 p-5">
         <p>
-          © {new Date().getFullYear()} Parcoil Network. All rights reserved.
+          © {new Date().getFullYear()} Pls Don't Sue!
         </p>
         <div className="gap-2 flex justify-center">
           {import.meta.env.DEV && (
